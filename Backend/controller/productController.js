dff9f4cf08118cc6e2f21758101d84aaab93ac6a
AhsanDeveloper764 <ahsanalise226@gmail.com>
2025-12-21 18:46:46 +0500

    update

---
+++ Backend/controller/productController.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const productSchema = require("../models/productsSchema")
+const {BACKEND_SERVER_PATH} = require("../config/index")
+const joi = require("joi")
+
+const productController = {
+    async createProduct(req,resp,next){
+
+    },
+    async getProduct(req,resp,next){
+
+    },
+    async GetById(req,resp,next){
+
+    },
+}
+
+module.exports = productController
---
+++ Backend/models/productsSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    
+    productname:{type:String,required:true},
+    price:{type:Number,required:true},
+    discount:{type:Number,required:true},
+    color:{type:String,required:true},
+    quantity:{type:Number,required:true},
+    image:{type:String,required:true}
+
+},{timestamps:true})
+
+const product = mongoose.model("product",productSchema,"Product")
+module.exports=product;
