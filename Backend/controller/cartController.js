c47f2f3ae941af31f59620642fff8b8ffc1b25ff
AhsanDeveloper764 <ahsanalise226@gmail.com>
2026-01-04 20:04:56 +0500

    update

--- Backend/controller/cartController.js
+++ Backend/controller/cartController.js
@@ -1,40 +1,54 @@
 const Joi = require("joi");
 const fs = require("fs");
 const cartSchema = require("../models/cartSchema");
-const userSchema = require("../models/userSchema");
 const productSchema = require("../models/productsSchema");
 const mongodbIdPattern = /^[a-fA-F0-9]{24}$/;
 
 const cartController = {
     async addCart(req,resp,next){
         const Cartvalidation = Joi.object({
-            id:Joi.string().regex(mongodbIdPattern).required()
+            productId:Joi.string().regex(mongodbIdPattern).required(),
+            qunatity:Joi.number().min(1).required()
         })
         const {error} = Cartvalidation.validate(req.body);
         if(error){
             return next(error)
         }
-        let cartDetail;
+
+        const userId = req.user._id
+        const {productId,qunatity} = req.body;
         try {
-            const {id} = req.params
-            cartDetail = await userSchema.find({_id:id})
-        } catch (error) {
-            return next(error)
-        }
-        let productId;
-        try {
-            const {id} = req.params
-            productId = await productSchema.find({_id:id})
-            if(productId){
-                const cart = cartSchema({
-                    quantity,
+            const product = await productSchema.findById(productId)
+            if(!product){
+                return resp.status(404).json({
+                    success:false,
+                    message:"Product Not Found"
                 })
-                await cart.save()
-            }   
-        } catch (error) {
+            }
+            let cart = await cartSchema.findById({userId})
+            if(!cart){
+                cart = new cartSchema({
+                    userId,
+                    items:[{productId,qunatity}]
+                }) 
+            }else{
+                const itemIndex = cartSchema.items.findIndex((item)=>{
+                item.productId.toString() === productId })
+                if(itemIndex > -1){
+                    cart.items[itemIndex].quantity += qunatity
+                }else{
+                    cart.items.push({productId,qunatity})
+                }
+            }
+            await cart.save()
+            resp.status(201).json({
+                success:true,
+                message:"Added to Cart",
+                response:cart
+            })
+        }catch(error){
             return next(error)
         }
     }
 }
-
 module.exports = cartController
